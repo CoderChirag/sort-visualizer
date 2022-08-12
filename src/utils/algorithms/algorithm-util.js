a7678a7c469b5cbed7d3a175c986d36619906f13
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-08-12 12:37:32 +0530

    Add Algorithm Helper Functions

---
+++ src/utils/algorithms/algorithm-util.js
@@ -0,0 +1,50 @@
+export const newStackTrace = arr => {
+	return [
+		{
+			arr: [...arr],
+			functionalityA: [],
+			functionalityB: [],
+			functionalityC: [],
+			functionalityD: [],
+			sortedIndices: [],
+		},
+	];
+};
+
+export const addToStackTrace = (
+	stackTrace,
+	arr,
+	sortedIndices = [],
+	functionalityA = [],
+	functionalityB = [],
+	functionalityC = [],
+	functionalityD = []
+) => {
+	stackTrace.push({
+		arr: [...arr],
+		functionalityA: [...functionalityA],
+		functionalityB: [...functionalityB],
+		functionalityC: [...functionalityC],
+		functionalityD: [...functionalityD],
+		sortedIndices: [...sortedIndices],
+	});
+};
+
+export const getLastSortedIndices = stackTrace => {
+	return stackTrace[stackTrace.length - 1].sortedIndices;
+};
+
+export const swap = (arr, i, j) => {
+	arr[i] = arr[i] ^ arr[j];
+	arr[j] = arr[i] ^ arr[j];
+	arr[i] = arr[i] ^ arr[j];
+};
+
+export const createKey = (
+	functionalityA,
+	functionalityB,
+	functionalityC,
+	functionalityD
+) => {
+	return { functionalityA, functionalityB, functionalityC, functionalityD };
+};
